48b44c2b5e152e7bcb7762696a11be9cfdd72744
Kyle Sandell <kasandell@gmail.com>
2017-01-28 23:07:26 -0800

    fixed up compression (I think)

--- compression/decompress.cpp
+++ compression/decompress.cpp
@@ -8,19 +8,18 @@ using namespace std;
 int main()
 {
     string lookup = "./:abcghklmopstw";
-    string read;
     ifstream inf("hi.txt");
-    getline(inf, read);
-    inf.close();
-    for(auto&c: read)
+    while(!inf.eof() && inf.good())
     {
-        cout << static_cast<unsigned int>(c) << endl;
+        unsigned int c;
+        
+        inf >> c;
         unsigned int c1 = c >> 4;
         unsigned int c2 = c & 0x0F;
-        cout << c1 << " " << c2 << endl;
-        //cout << lookup[c1] << lookup[c2];
+        cout << lookup[c1] << lookup[c2];
     }
     cout << endl;
+    inf.close();
     return 0;
 
 
--- compression/main.cpp
+++ compression/main.cpp
@@ -26,42 +26,10 @@ int main()
     for(int i = 0; i < addrs.size(); i+=2)
     {
         uint8_t c = (addrs[i] << 4) | (addrs[i+1] & 0x0F);
-        out << static_cast<char>(c);
+        cout << static_cast<unsigned int>(c)<< " ";
+        out << static_cast<unsigned int>(c) << " ";
     }
     cout << endl;
-    /*
-    vector< bitset<4> > bits;
-    for(const auto& i: addrs)
-    {
-        bitset<4> bts(i);
-        bits.push_back(bts);
-    }
-    vector<string> rep;
-    for(const auto& b: bits)
-    {
-        rep.push_back(b.to_string());
-    }
-    string str1 = accumulate(rep.begin(), rep.end(), string(""));
-    if(str1.length() % 8) 
-    {
-        str1.append("0000");
-    }
-    vector<char> out;
-    for(int i = 0; i < str1.length(); i += 8)
-    {
-        string s = str1.substr(i, 8);
-        bitset<8> b(s);
-        auto x = b.to_ulong();
-        char c = static_cast<unsigned char>(x);
-        cout << static_cast<unsigned int>(c) << endl;
-        out.push_back(c);
-    }
-    string fin(out.begin(), out.end());
-    ofstream outf("hi.txt");
-    outf << fin;
-    outf.close();
-    */
-
-    //bitset<str.length()> bts(str);
+    out.close();
     return 0;
 }
